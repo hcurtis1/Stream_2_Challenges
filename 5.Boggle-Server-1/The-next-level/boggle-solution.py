ea73c449678cb06ceb62b76513d5af7889b3f34f
Henry Curtis <hcuk106@gmail.com>
2017-05-24 11:05:57 +0100

    updated display word function

--- 5.Boggle-Server-1/The-next-level/boggle-solution.py
+++ 5.Boggle-Server-1/The-next-level/boggle-solution.py
@@ -53,13 +53,15 @@ def get_dictionary(words):
     with open(words) as f:
         return [w.strip().upper() for w in f]
 
+def display_words(words):
+    for word in words:
+        print word
+    print "Found {0} words".format(len(words))
 
 def main():
     grid = make_grid(3, 3)
     dictionary = get_dictionary('/usr/share/dict/words')
     words = search(grid, dictionary)
-    for word in words:
-        print word
-    print "Found {0} words".format(len(words))
+    display_words(words)
 
 main()
