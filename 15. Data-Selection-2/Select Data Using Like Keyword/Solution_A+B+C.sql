95fcf7d73d4a50f22358c98905cfbc3ef1e49af9
Henry Curtis <hcuk106@gmail.com>
2017-06-10 12:15:20 +0100

    Initial Commit

---
+++ 15. Data-Selection-2/Select Data Using Like Keyword/Solution_A+B+C.sql
@@ -0,0 +1,9 @@
+use mydb;
+
+select * from articles where title like '%article%';
+
+select * from articles where content like '%more%';
+
+select * from articles where content like 'wow%';
+
+select * from articles where title like '%4';
